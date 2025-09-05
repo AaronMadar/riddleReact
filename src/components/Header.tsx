292054659d106e91ed52e922160d6244b9fc4487
Aaron <aaronmadar2655@gmail.com>
2025-09-05 17:35:42 +0300

    component header with functions of button home & login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
-export default function Header(){
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
+export default function Header() {
+    const navigate = useNavigate();
+    const { isAuthenticated, logout } = useContext(AuthContext);
 
     return (
-        <header></header>
-    )
+        <header>
+
+            <button onClick={() => navigate("/")}>Home</button>
+
+
+            {!isAuthenticated && (<button onClick={() => navigate("/login")}>Login</button>)}
+            {isAuthenticated && (<button
+                    onClick={() => {
+                        logout();
+                        navigate("/"); // retourne à Home après logout
+                    }}
+                >
+                    Logout
+                </button>
+            )}
+        </header>
+    );
 }
