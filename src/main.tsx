94d8bcdb5f6ba5acc13151d3e0ce7891f9777031
Aaron <aaronmadar2655@gmail.com>
2025-09-05 17:40:36 +0300

    add layout in main to put the header in all app

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import HomePage from './Pages/HomePage.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import LoginPage from './Pages/LoginPage.tsx'
-import AuthProvider from './context/AuthContext.tsx'
-import SubscribePage from './Pages/SubscribePage.tsx'
-import GamePage from './Pages/GamePage.tsx'
-import LevelPage from './Pages/LevelPage.tsx'
-import LevelProvider from './context/LevelContext.tsx'
-import AdminPage from './Pages/AdminPage.tsx'
-import Create from './Pages/CreatePage.tsx'
-import Read from './Pages/ReadPage.tsx'
-import Update from './Pages/UpdatePage.tsx'
-import Delete from './Pages/DeletePage.tsx'
-import LeaderBoard from './Pages/Leaderboard.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthProvider from './context/AuthContext.tsx';
+import LevelProvider from './context/LevelContext.tsx';
+import Header from './components/Header.tsx';
+
+import HomePage from './Pages/HomePage.tsx';
+import LoginPage from './Pages/LoginPage.tsx';
+import SubscribePage from './Pages/SubscribePage.tsx';
+import GamePage from './Pages/GamePage.tsx';
+import LevelPage from './Pages/LevelPage.tsx';
+import AdminPage from './Pages/AdminPage.tsx';
+import Create from './Pages/CreatePage.tsx';
+import Read from './Pages/ReadPage.tsx';
+import Update from './Pages/UpdatePage.tsx';
+import Delete from './Pages/DeletePage.tsx';
+import LeaderBoard from './Pages/Leaderboard.tsx';
+
+// 1️ Crée un Layout avec Header + Outlet
+import { Outlet } from 'react-router-dom';
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -22,20 +37,22 @@ createRoot(document.getElementById('root')!).render(
       <LevelProvider>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/subscribe' element={<SubscribePage />} />
-            <Route path='/game' element={<GamePage />} />
-            <Route path='/levelselection' element={<LevelPage />} />
-            <Route path='/adminpage' element={<AdminPage />} />
-            <Route path='/leaderboard' element={<LeaderBoard />} />
-            <Route path='/createnewriddle' element={<Create />} />
-            <Route path='/getallriddle' element={<Read />} />
-            <Route path='/updateriddle' element={<Update />} />
-            <Route path='/deleteriddle' element={<Delete />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="subscribe" element={<SubscribePage />} />
+              <Route path="game" element={<GamePage />} />
+              <Route path="levelselection" element={<LevelPage />} />
+              <Route path="adminpage" element={<AdminPage />} />
+              <Route path="leaderboard" element={<LeaderBoard />} />
+              <Route path="createnewriddle" element={<Create />} />
+              <Route path="getallriddle" element={<Read />} />
+              <Route path="updateriddle" element={<Update />} />
+              <Route path="deleteriddle" element={<Delete />} />
+            </Route>
           </Routes>
         </BrowserRouter>
       </LevelProvider>
     </AuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
